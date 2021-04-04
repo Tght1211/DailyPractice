8aefa8a5b6db210d5a9a075e000d55b7a825a0a5
Tght <17674009984@163.com>
2021-04-04 15:57:49 +0800

    日常练习

---
+++ day11/demo17_test/Test.java
@@ -0,0 +1,11 @@
+package day11.demo17_test;
+
+import day11.demo17_bean.Customer;
+import day11.demo17_view.CustomerView;
+
+public class Test {
+    public static void main(String[] args) {
+        CustomerView view = new CustomerView();
+        view.menu();
+    }
+}
